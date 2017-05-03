339881432cb9fd989ea4dc95f31e9a2f46f2e76c
brunocalou <brunocaloualves@gmail.com>
2017-05-03 00:55:01 -0300

    Added hability to fire bullets

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,17 +6,27 @@ public class PlayerMovement : NetworkBehaviour
 {
     public float maxSpeed = 30f;
     public float maxRotateSpeed = 3f;
-	public float acceleration = 20;
+	public float acceleration = 30;
 	public float drag = 50;
 
-	private float speed = 0;
-	private Vector3 lastPosition;
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
+	private float m_Speed = 0;
+	private Vector3 m_LastPosition;
+	private Rigidbody m_Rigidbody;  
+
+	private void Awake ()
+	{
+		m_Rigidbody = GetComponent<Rigidbody> ();
+	}
 
     void FixedUpdate ()
     {
 		if (!isLocalPlayer) {
 			return;
 		}
+
         // Store the input axes.
         float h = Input.GetAxis ("Horizontal");
         float v = Input.GetAxis ("Vertical");
@@ -25,33 +35,41 @@ public class PlayerMovement : NetworkBehaviour
         Move (v);
 
         // Rotate the player
-        Rotate(h * v);
+		Rotate(h * m_Speed / maxSpeed);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			CmdFire();
+		}
+
     }
 
     void Move (float v)
     {
-		float measuredSpeed = (transform.position - lastPosition).magnitude / Time.deltaTime * (speed > 0 ? 1 : -1);
-		speed = measuredSpeed;
+		float measuredSpeed = (transform.position - m_LastPosition).magnitude / Time.deltaTime * (m_Speed > 0 ? 1 : -1);
+		m_Speed = measuredSpeed;
 
-		speed += acceleration * Time.deltaTime * v;
+		m_Speed += acceleration * Time.deltaTime * v;
 
 		// If the speed and the user joystick have different directions, or if v is zero, add the drag
-		if (v * speed <= 0) {
-			speed -= drag * Time.deltaTime * speed / maxSpeed;
+		if (v * m_Speed <= 0) {
+			m_Speed -= drag * Time.deltaTime * m_Speed / maxSpeed;
 
-			if (Mathf.Abs(speed) < 0.5)
-				speed = 0;
+			if (Mathf.Abs(m_Speed) < 0.5)
+				m_Speed = 0;
 		}
 
-		if (speed > maxSpeed) {
-			speed = maxSpeed;
-		} else if (speed < -maxSpeed) {
-			speed = -maxSpeed;
+		if (m_Speed > maxSpeed) {
+			m_Speed = maxSpeed;
+		} else if (m_Speed < -maxSpeed) {
+			m_Speed = -maxSpeed;
 		}
 
-		lastPosition = transform.position;
+		m_LastPosition = transform.position;
 
-        transform.position += transform.forward * Time.deltaTime * speed;
+		Vector3 movement = transform.forward * m_Speed * Time.deltaTime;
+
+		m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
     }
 
     void Rotate (float h)
@@ -60,6 +78,26 @@ public class PlayerMovement : NetworkBehaviour
 
         transform.Rotate(0, rotation, 0);
     }
+
+	[Command]
+	void CmdFire()
+	{
+
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate (
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = 50 * bullet.transform.forward;
+		bullet.GetComponent<Rigidbody> ().position += transform.forward.normalized;
+		// Spawn the bullet to the Clients
+		NetworkServer.Spawn(bullet);
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);
+	}
 
 	public override void OnStartLocalPlayer()
 	{
