ca5a5e2edd8f13aad75bbdd632c454e6c53054d0
brunocalou <brunocaloualves@gmail.com>
2017-05-07 17:36:45 -0300

    Added health, reverted movement to be simple again, added map boundaries to kill the player, added respawn points

---
+++ Assets/Scripts/InstantKill.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantKill : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+	{
+		var hit = collision.gameObject;
+		var health = hit.GetComponent<Health>();
+		if (health != null)
+		{
+			health.TakeDamage(health.getMaxHealth());
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,21 +4,22 @@ using UnityStandardAssets.Utility;
 
 public class PlayerMovement : NetworkBehaviour
 {
-    public float maxSpeed = 30f;
-    public float maxRotateSpeed = 3f;
-	public float acceleration = 30;
-	public float drag = 50;
+    public float maxSpeed = 25f;
+    public float maxRotateSpeed = 2f;
+//	public float acceleration = 30;
+//	public float drag = 50;
 
 	public GameObject bulletPrefab;
 	public Transform bulletSpawn;
 
-	private float m_Speed = 0;
+//	private float m_Speed = 25f;
 	private Vector3 m_LastPosition;
 	private Rigidbody m_Rigidbody;  
 
 	private void Awake ()
 	{
 		m_Rigidbody = GetComponent<Rigidbody> ();
+//		m_Rigidbody.drag = 1f;
 	}
 
     void FixedUpdate ()
@@ -32,55 +33,80 @@ public class PlayerMovement : NetworkBehaviour
         float v = Input.GetAxis ("Vertical");
 
         // Move the player around the scene.
-        Move (v);
+		Move (v);
 
         // Rotate the player
-		Rotate(h * m_Speed / maxSpeed);
+//		Rotate(h * m_Speed / maxSpeed);
+		Rotate(h * maxRotateSpeed * (v < 0 ? -1: 1));
 
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetButtonDown("Fire1"))
 		{
-			CmdFire();
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit floorHit;
+
+			if (Physics.Raycast (ray, out floorHit)) {
+				// Create a vector from the player to the point on the floor the raycast from the mouse hit.
+				Vector3 playerToMouse = floorHit.point - transform.position;
+
+				// Ensure the vector is entirely along the floor plane.
+				playerToMouse.y = 0f;
+
+				// Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+//				Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+
+//				print (Vector3.Angle (playerToMouse, transform.forward));
+				if (Vector3.Angle (playerToMouse, transform.forward) < 45) {
+					CmdFire (playerToMouse.normalized);
+				}
+			}
+//			print ("Fire");
 		}
+
+//		if (Input.GetKeyDown(KeyCode.Space))
+//		{
+//			CmdFire(transform.forward);
+//		}
 
     }
 
     void Move (float v)
     {
-		float measuredSpeed = (transform.position - m_LastPosition).magnitude / Time.deltaTime * (m_Speed > 0 ? 1 : -1);
-		m_Speed = measuredSpeed;
+//		float measuredSpeed = (transform.position - m_LastPosition).magnitude / Time.deltaTime * (m_Speed > 0 ? 1 : -1);
+//		m_Speed = measuredSpeed;
+//
+//		m_Speed += acceleration * Time.deltaTime * v;
+//
+//		// If the speed and the user joystick have different directions, or if v is zero, add the drag
+//		if (v * m_Speed <= 0) {
+//			m_Speed -= drag * Time.deltaTime * m_Speed / maxSpeed;
+//
+//			if (Mathf.Abs(m_Speed) < 0.5)
+//				m_Speed = 0;
+//		}
+//
+//		if (m_Speed > maxSpeed) {
+//			m_Speed = maxSpeed;
+//		} else if (m_Speed < -maxSpeed) {
+//			m_Speed = -maxSpeed;
+//		}
+//
+//		m_LastPosition = transform.position;
 
-		m_Speed += acceleration * Time.deltaTime * v;
-
-		// If the speed and the user joystick have different directions, or if v is zero, add the drag
-		if (v * m_Speed <= 0) {
-			m_Speed -= drag * Time.deltaTime * m_Speed / maxSpeed;
-
-			if (Mathf.Abs(m_Speed) < 0.5)
-				m_Speed = 0;
-		}
-
-		if (m_Speed > maxSpeed) {
-			m_Speed = maxSpeed;
-		} else if (m_Speed < -maxSpeed) {
-			m_Speed = -maxSpeed;
-		}
-
-		m_LastPosition = transform.position;
-
-		Vector3 movement = transform.forward * m_Speed * Time.deltaTime;
-
+		Vector3 movement = transform.forward * maxSpeed * v * Time.deltaTime;
+//
 		m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+
+//		m_Rigidbody.AddForce (transform.forward * v * m_Speed, ForceMode.Force);
     }
 
     void Rotate (float h)
     {
-		float rotation = h * maxRotateSpeed;
-
-        transform.Rotate(0, rotation, 0);
+		transform.Rotate (0, h * maxRotateSpeed, 0);
+//		m_Rigidbody.AddTorque(0, h * maxRotateSpeed, 0, ForceMode.VelocityChange);
     }
 
 	[Command]
-	void CmdFire()
+	void CmdFire(Vector3 forward)
 	{
 
 		// Create the Bullet from the Bullet Prefab
@@ -89,9 +115,11 @@ public class PlayerMovement : NetworkBehaviour
 			bulletSpawn.position,
 			bulletSpawn.rotation);
 
+		Physics.IgnoreCollision (bullet.GetComponent<Collider> (), GetComponent<Collider> ());
+
 		// Add velocity to the bullet
-		bullet.GetComponent<Rigidbody>().velocity = 50 * bullet.transform.forward;
-		bullet.GetComponent<Rigidbody> ().position += transform.forward.normalized;
+		bullet.GetComponent<Rigidbody>().velocity = 50 * forward;
+//		bullet.GetComponent<Rigidbody> ().position += forward.normalized;
 		// Spawn the bullet to the Clients
 		NetworkServer.Spawn(bullet);
 
