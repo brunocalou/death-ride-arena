d2021a1b434f3de456c019480b20435e8b90fa30
brunocalou <brunocaloualves@gmail.com>
2017-06-14 14:09:04 -0300

    Changed network menu to hide when pressing the ESC key

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	bool isActive = true;
+	GameObject menu;
+
+	void Start () {
+		this.menu = GameObject.Find ("Network Manager");
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			toggleMenu ();
+		}
+	}
+	
+	void toggleMenu() {
+		if (this.menu != null) {
+			this.isActive = !this.isActive;
+			this.menu.SetActive (this.isActive);
+		}
+	}
+}
